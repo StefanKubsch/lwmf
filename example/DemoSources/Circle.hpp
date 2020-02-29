cc18c819729d7ec68e7b80b72302b58b00e00a6d
StefanKubsch <stefan.kubsch@outlook.de>
2020-02-29 18:02:52 +0100

    Code cleanup

--- example/DemoSources/Circle.hpp
+++ example/DemoSources/Circle.hpp
@@ -25,7 +25,7 @@ namespace Circle
                 const float aFactor{ a * lwmf::RAD2DEG };
                 const float RoundNoiseFactor{ 70.0F + Ring.Noise(ScreenTexture.Width + std::cosf(TimeFactor) + std::cosf(aFactor), ScreenTexture.Height + std::sinf(TimeFactor) + std::sinf(aFactor), 0.0F) * 150.0F * (1.0F + std::sinf((Time + 270.0F) * lwmf::RAD2DEG)) };
 
-                lwmf::SetPixel(ScreenTexture,
+                lwmf::SetPixelSafe(ScreenTexture,
                     static_cast<std::int_fast32_t>(ScreenTexture.WidthMid + (std::cosf(aFactor) * RoundNoiseFactor)),
                     static_cast<std::int_fast32_t>(ScreenTexture.HeightMid + (std::sinf(aFactor) * RoundNoiseFactor)),
                     lwmf::RGBAtoINT(static_cast<std::int_fast32_t>(a + Time) % 360 - (ColorMod << 1), static_cast<std::int_fast32_t>(a + Time) % 100 - ColorMod, static_cast<std::int_fast32_t>(a + Time) % 270, 255));
