1de2fa115e7570b0f041558c31d3145c735b6bbd
StefanKubsch <stefan.kubsch@outlook.de>
2020-09-11 20:26:06 +0200

    Updated plasma demo part

--- example/DemoSources/Plasma.hpp
+++ example/DemoSources/Plasma.hpp
@@ -2,41 +2,38 @@
 
 #include <cstdint>
 #include <cmath>
-#include <array>
-#include <utility>
 
 namespace Plasma
 {
 
 
-	inline void Draw()
-	{
-		static std::array<std::pair<std::int_fast32_t, std::int_fast32_t>, 3> Plasmas{ { {5, 2}, {125, 1}, {250,3} } };
+    inline void Draw()
+    {
+        static float TimeElapsed{};
+        TimeElapsed += 0.0166F;
 
-		for (auto& Plasma : Plasmas)
-		{
-			if (Plasma.first > 255 || Plasma.first <= 1)
-			{
-				Plasma.second = -Plasma.second;
-			}
+        #pragma omp parallel for
+        for (std::int_fast32_t y{}; y < ScreenTexture.Height; y += 2)
+        {
+            const float TempY{ (0.5F + y / static_cast<float>(ScreenTexture.Height) - 1.0F) * 20.0F - 10.0F };
 
-			Plasma.first += Plasma.second;
-		}
+            for (std::int_fast32_t x{}; x < ScreenTexture.Width; x += 2)
+            {
+                const float TempX{ (0.5F + x / static_cast<float>(ScreenTexture.Width) - 1.0F) * 20.0F - 10.0F };
+                const float cx{ TempX + 10.0F * (std::sinf(TimeElapsed * 0.33F)) };
+                const float cy{ TempY + 10.0F * (std::cosf(TimeElapsed * 0.5F)) };
+                const float Value{ (std::sinf(TempY + TimeElapsed) + std::sinf((TempX + TimeElapsed) * 0.5F) + std::sinf((TempX + TempY + TimeElapsed) * 0.5F) + ((std::sinf(std::sqrtf(cx * cx + cy * cy + 1.0F) + TimeElapsed)) * 0.5F)) * lwmf::PI };
 
-		#pragma omp parallel for
-		for (std::int_fast32_t y{ 115 }; y < ScreenTexture.Height; ++y)
-		{
-			const float TempY{ 64.0F * std::cosf(y / 64.0F) };
+                const std::int_fast32_t Red{ static_cast<std::int_fast32_t>((std::sinf(Value) * 0.5F + 0.5F) * 255.0F) };
+                const std::int_fast32_t Green{ static_cast<std::int_fast32_t>((std::sinf(Value + lwmf::DoublePI * 0.33F) * 0.5F + 0.5F) * 255.0F) };
+                const std::int_fast32_t Blue{ static_cast<std::int_fast32_t>((std::sinf(Value + 4.0F * lwmf::PI * 0.33F) * 0.5F + 0.5F) * 255.0F) };
 
-			for (std::int_fast32_t x{}; x < ScreenTexture.Width; ++x)
-			{
-				const std::int_fast32_t Color{ static_cast<std::int_fast32_t>((128.0F * std::sinf(x / 64.0F)) + TempY) };
-				lwmf::SetPixel(ScreenTexture, x, y, lwmf::RGBAtoINT((Color + Plasmas[2].first) & 255, (Color + Plasmas[0].first) & 255, (Color + Plasmas[1].first) & 255, 255));
-			}
-		}
+                lwmf::SetPixel(ScreenTexture, x, y, lwmf::RGBAtoINT(Red, Green, Blue, 255));
+            }
+        }
 
-		DisplayInfoBox("OpenMP accelerated realtime plasma");
-	}
+        DisplayInfoBox("OpenMP accelerated RGB plasma");
+    }
 
 
 } // namespace Plasma
